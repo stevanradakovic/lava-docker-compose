1c39f7673aa0ce188e5b938aaf21e69c98d226a1
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2019-02-15 17:44:17 +0000

    Fixes to get SQUAD-LAVA communication working
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- squad/setup.sh
+++ squad/setup.sh
@@ -8,6 +8,4 @@ ADMIN_TOKEN_STRING=$(squad-admin drf_create_token ${SQUAD_ADMIN_USER})
 ADMIN_TOKEN=$(echo ${ADMIN_TOKEN_STRING} | cut -d ' ' -f 3)
 
 # create usergroup, group, project and CI backend using API
-# django URLField doesn't accept 'lava_server' as address. Convert to IP address.
-LAVA_SERVER_IP=$(getent hosts lava_server | awk '{ print $1 }')
-python3 create_objects.py --url http://squad-web:8000/api/ --token "${ADMIN_TOKEN}" --lava-url http://${LAVA_SERVER_IP}/RPC2/ --lava-username ${LAVA_ADMIN_USER} --lava-token ${LAVA_SQUAD_TOKEN}
+python3 create_objects.py --url http://squad-web:8000/api/ --token "${ADMIN_TOKEN}" --lava-url http://lava.server/RPC2/ --lava-username ${LAVA_SQUAD_USER} --lava-token ${LAVA_SQUAD_TOKEN}
